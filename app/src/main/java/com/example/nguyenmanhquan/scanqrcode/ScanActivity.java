632fb8669714390f2426db5005a8e7dd6fec5812
Manh Quan <nguyen.manh.quan@framgia.com>
2017-09-25 08:58:14 +0700

    First commit

---
+++ app/src/main/java/com/example/nguyenmanhquan/scanqrcode/ScanActivity.java
@@ -0,0 +1,63 @@
+package com.example.nguyenmanhquan.scanqrcode;
+
+import android.app.Application;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by nguyen.manh.quan on 20-Sep-17.
+ */
+
+public class ScanActivity extends AppCompatActivity implements BarcodeReader.BarcodeReaderListener {
+
+    BarcodeReader barcodeReader;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // get the barcode reader instance
+        barcodeReader = (BarcodeReader) getSupportFragmentManager().findFragmentById(R.id.barcode_scanner);
+    }
+
+    @Override
+    public void onScanned(Barcode barcode) {
+
+        // playing barcode reader beep sound
+        barcodeReader.playBeep();
+
+        // ticket details activity by passing barcode
+        Intent intent = new Intent(ScanActivity.this, TicketResultActivity.class);
+        intent.putExtra("code", barcode.displayValue);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onScannedMultiple(List<Barcode> list) {
+
+    }
+
+    @Override
+    public void onBitmapScanned(SparseArray<Barcode> sparseArray) {
+
+    }
+
+    @Override
+    public void onScanError(String s) {
+        Toast.makeText(getApplicationContext(), "Error occurred while scanning " + s, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
